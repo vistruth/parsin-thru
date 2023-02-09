1502d0de1e1c903e6c4a1dc3c8c2f09fdc534455
vistruth <117324772+vistruth@users.noreply.github.com>
2023-02-08 20:44:07 -0500

    Added regular log auto parser
    Added regular log parser and exit Menu.

--- parsin-thru.py
+++ parsin-thru.py
@@ -3,7 +3,8 @@
 ### IMPORT STATEMENTS ###
 import sys
 import os
-
+import gzip
+import re
 
 ###  Funtions ###
 
@@ -63,6 +64,7 @@ def main_menu():
     else:
       print("\n\nYeah, sorry, that is not going to work! Please choose again!")
       main_menu() 
+#Pcap mennu
 def pcap_options():
   print("\n\n")
   print("Pcap type files")
@@ -94,6 +96,7 @@ def pcap_expl():
     if option =="1":
       pcap_options()
     elif option =="2":
+      print("\n\nExiting... Thank you for using Parsin-Thru!")
       sys.exit()
     else:
       print("Yeah, sorry, that is not going to work! Please choose again!")
@@ -110,19 +113,22 @@ def diff_expl():
     if option =="1":
       main_menu()
     elif option =="2":
+      print("\n\nExiting... Thank you for using Parsin-Thru!")
       sys.exit()
     else:
       print("Yeah, sorry, that is not going to work! Please choose again!")
       diff_expl()
+# Pcap auto parser and extractor
 def tshark_cmds():  
-  file = input ("Enter file name EXACTLY here: ")
+  file = input ("Enter Pcap file name EXACTLY here: ")
   tshark_extract = os.system("tshark -r " + file + " --export-objects 'http,exported' -q")
   print("Extracting files to exported folder...")
   tshark_parse = os.system("tshark -r " + file + " -q -z ip_hosts,tree")
   print("Number of files extracted from " + file)
   file_count = os.system("find exported -type f | wc -l")
   return tshark_extract, tshark_parse, file_count, file_count
-
+  exit()
+# regular log file menu
 def reg_logs():
   print("\n\nRegular log Options")
   print("__"*35)
@@ -137,16 +143,12 @@ def reg_logs():
     elif option=="3":
       main_menu()
     elif option=="4":
+      print("\n\nExiting... Thank you for using Parsin-Thru!")  
       sys.exit()
     else:
       print("Yeah, sorry, that is not going to work! Please choose again!")
       reg_logs()
-#auto log parser  
-def log_parse():
-  file = input("Enter log file name EXACTLY")
-  run_py = os.system("./log.py "+ file)
-  return run_py
-# and basic explaination of parse logging
+# basic explaination of parse logging
 def how_parse():
   print("\n\nBasics how to parse with aux cut etc.\nWe can use awk or cut Linux commands to parse our log files. We can get count of unique IP addresses that generated most or least traffic from the log file. It also can get sorted. We need to find a delimeter of the log file we are using log if we use 'cut' command. Adding grep allows us to get a strong tool for carving out data. For use of awk command as well as the cut command we need to know position (field number) of the IP address or other columns in the file.   ")
   print("\n\n"+"__"*35)
@@ -157,8 +159,59 @@ def how_parse():
     if option =="1":
       reg_logs()
     elif option =="2":
+      print("\n\nExiting... Thank you for using Parsin-Thru!")
       sys.exit()
     else:
       print("Yeah, sorry, that is not going to work! Please choose again!")
       how_parse()
+#regular log auto parser
+def log_parse():
+  ip_dict = {}
+  log= input("Enter Log file name EXACTLY: ")
+  #automatically detects if log file is gzip and unzips 
+  if os.path.splitext(log)[1] == ".gz":
+    f = gzip.open(log,'rt')
+  else:
+    f = open(log)
+  for line in f:
+    line = line.strip()
+    pattern = re.findall(r'(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})', line)
+    if pattern is not None and len(pattern) != 0:
+      ip = pattern[0].strip()
+      # ignore black ip addresses
+      if ip != "0.0.0.0":
+        ip_dict[ip] = ip_dict.get(ip, 0) + 1
+# Count up a summation of all dictionary values
+  total = 0
+  for entry in ip_dict:
+    total += ip_dict[entry]
+  # prints top
+  print("_"*40)
+  print(f'| Percent |  Count |{"":>16}IP | \n {"_":_>39}')
+  #loop through ips to get a count
+  for entry in sorted(ip_dict.items(), key = lambda x: (x[1], x[0])):
+  # Convert the amount of times appeared into a percent and then a string
+    percent = entry[1] / total * 100
+    percent = str(percent)
+    # percent is the percent entry[1] is the count and entry[0] is the ip addresses
+    print(f'|  {percent[:7]:0<7} |{entry[1]:>6} |   {entry[0]:>15} |')
+  #prints  bottom
+  print("_"*40)
+  print(f'|{"Total":>9} |{total:>6} |{"":>19}|')
+  print("_"*40)
+  f.close()
+  exit()
+def exit():
+  print("\n\nIs there anything else you need?")
+  print("\n\n"+"__"*35)
+  option="0"
+  while option =="0":
+    print("1 - Yes! Please return to main menu.\n2 - No thanks, please exit.")
+    option = input ("Please choose between 1 and 2: ")
+    if option =="1":
+      print("Returning to main menu...")
+      main_menu()
+    elif option =="2":
+      print("\n\nExiting... Thank you for using Parsin-Thru!")
+      sys.exit()
 intro()
